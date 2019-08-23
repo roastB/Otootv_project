dce2a2980982e4da089eb74c6645b154e831e44b
keumhyewon <37867271+keumhyewon@users.noreply.github.com>
2019-08-24 06:44:03 +0900

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	README.md

--- main/views.py
+++ main/views.py
@@ -19,6 +19,27 @@ def transferview1(request):
 class transferview2(ListView):
     model = Notice
 
+
+
+
+
+class main_logo_pageView(TemplateView):
+    template_name = 'main_logo_page.html'
+class signup_1View(TemplateView):
+    template_name = 'signup_1.html'
+class signup_2View(TemplateView):
+    template_name = 'signup_2.html'
+class login_1View(TemplateView):
+    template_name = 'login_1.html'
+class login_2View(TemplateView):
+    template_name = 'login_2.html'
+class subscribe_1View(TemplateView):
+    template_name = 'subscribe_1.html'
+class subscribe_2View(TemplateView):
+    template_name = 'subscribe_2.html'
+class subscribe_3View(TemplateView):
+    template_name = 'subscribe_3.html'
+
 class mainView(TemplateView):
     template_name = 'main.html'
 
@@ -30,3 +51,23 @@ class channelView(TemplateView):
 
 class videoView(TemplateView):
     template_name = 'video.html'
+
+class noticeView(TemplateView):
+    template_name = 'notice.html'
+
+class helpView(TemplateView):
+    template_name = 'help.html'
+
+class accountView(TemplateView):
+    template_name = 'account.html'
+
+from main.forms import PostForm
+def inquiry_newView(request):
+    form = PostForm()
+    return render(request, 'inquiry_new.html', {'form': form})
+
+class inquiryView(TemplateView):
+    template_name = 'inquiry.html'
+
+class inquiry_detailView(TemplateView):
+    template_name = 'inquiry_detail.html'
